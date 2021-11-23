830dba25aad5d77db3606963c310b5621cdb7bab
kang.dj <kang.dj@ensecure.co.kr>
2021-11-23 17:52:38 +0900

    백엔드 서버와 연결 및 코드 리팩토링

--- src/service/RequestAxios.js
+++ src/service/RequestAxios.js
@@ -15,21 +15,42 @@ class RequestAxios {
             }
         }).then(response => response).catch(error => {console.log(error)})
 
-        
+            return await this.sendResponseData(response);
+    }
 
+    static requestData = async (url, data, type, contentType) =>{
 
-        if(response === undefined){
-            alert("서버와의 연결이 원활하지 않습니다.")
-            return response;
-        } else {   
-            return response.status === 200 ? response.data : response;
+        //일단은 GET방식은 추가적인 파라미터가 없다고 생각하고 코드를 짬
+        if(type === undefined && contentType === undefined){
+          const response = await Axios.get(url)
+                .then(res => res)
+                .catch(error => {console.log(error)})
+
+                return await this.sendResponseData(response);
         }
+        else{
+          //POST 방식
+          const response = await Axios.post(url,data,{
+                type : type,
+                contentType : contentType
+          }).then(response => response).catch(error => {console.log(error)})
+          
+                return await this.sendResponseData(response);
 
-
+        }
     }
 
 
-
+    static sendResponseData = (response) =>{
+      if(response === undefined){
+        alert("서버와의 연결이 원활하지 않습니다.")
+        return response;
+      } 
+      else {
+        return response.status === 200 ? response.data : response;
+      }
+    }
+    
 }
 
 

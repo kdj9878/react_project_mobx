db6442bcdbffdfea3fb59a244960d34ee7631b3f
kang.dj <kang.dj@ensecure.co.kr>
2021-11-24 10:19:56 +0900

    회원 정보 코드와 라벨을 담고 있는 JS 추가

---
+++ src/context/context.js
@@ -0,0 +1,36 @@
+export const userContexts = [
+  {
+    code : "userNickname",
+    label : "사원 이름"
+  },
+  {
+    code : "userEmail",
+    label : "사원 이메일",
+  },
+  {
+    code : "userPh",
+    label : "사원 핸드폰 번호"
+  },
+  {
+    code : "deptNm",
+    label : "소속 부서"
+  },
+  {
+    code: "deptDtNm",
+    label : "소속 팀"
+  },
+  {
+    code : "userGender",
+    label : "성별"
+  },
+  {
+    code : "userAddr",
+    label : "주소"
+  },
+  {
+    code : "userDesc",
+    label : "사원 자기 소개"
+  }
+];
+
+
